fc1941ffc6c87abbfe2fa7a3bbe38caa18082962
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-06-26 14:04:29 -0300

    fix: routes to https

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -11,23 +11,23 @@
         @yield("title")
     </title>
 
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css') }}">
-    <link href="{{ asset('frontend/css/select2.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css', true) }}">
+    <link href="{{ asset('frontend/css/select2.min.css', true) }}" rel="stylesheet">
     <link  type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <link  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link href="{{ asset('admin/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ asset('admin/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/css/nucleo-icons.css', true) }}" rel="stylesheet" />
+    <link href="{{ asset('admin/css/nucleo-svg.css', true) }}" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ asset('admin/css/material-dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/material-dashboard.css', true) }}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 
     {{-- Owl Carousel --}}
-    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css', true) }}">
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 
-    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css', true) }}">
 </head>
 <body>
     @include('layouts.inc.frontnavbar')
@@ -44,11 +44,11 @@
     @yield('modals')
 
     <!-- Scripts -->
-    <script src="{{ asset('frontend/js/jquery-3.6.4.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/select2.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.mask.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery-3.6.4.min.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/select2.min.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/jquery.mask.min.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/owl.carousel.min.js', true) }}"></script>
 
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -88,8 +88,8 @@
 
     </script>
 
-    <script src="{{ asset('frontend/js/custom.js') }}"></script>
-    <script src="{{ asset('frontend/js/checkout.js') }}"></script>
+    <script src="{{ asset('frontend/js/custom.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/checkout.js', true) }}"></script>
 
     @if (session("status") && session("message"))
         <script>
