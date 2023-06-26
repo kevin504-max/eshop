52ff78fbd14af0b2414b739318aab08d474efc07
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-06-26 14:03:47 -0300

    fix: reset routes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,18 +9,18 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css', true) }}">
-    <link href="{{ asset('frontend/css/select2.min.css', true) }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css') }}">
+    <link href="{{ asset('frontend/css/select2.min.css') }}" rel="stylesheet">
     <link  type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <link  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link href="{{ asset('admin/css/nucleo-icons.css', true) }}" rel="stylesheet" />
-    <link href="{{ asset('admin/css/nucleo-svg.css', true) }}" rel="stylesheet" />
+    <link href="{{ asset('admin/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/css/nucleo-svg.css') }}" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ asset('admin/css/material-dashboard.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/material-dashboard.css') }}">
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
 </head>
 <body>
     <div class="wrapper">
@@ -35,16 +35,16 @@
     @yield('modals')
 
     <!-- Scripts -->
-    <script src="{{ asset('frontend/js/jquery-3.6.4.min.js', true) }}"></script>
-    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js', true) }}"></script>
-    <script src="{{ asset('frontend/js/select2.min.js', true) }}"></script>
-    <script src="{{ asset('frontend/js/jquery.mask.min.js', true) }}"></script>
+    <script src="{{ asset('frontend/js/jquery-3.6.4.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/select2.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery.mask.min.js') }}"></script>
 
-    <script src="{{ asset('admin/js/core/popper.min.js', true) }}"></script>
-    <script src="{{ asset('admin/js/core/bootstrap.min.js', true) }}"></script>
-    <script src="{{ asset('admin/js/plugins/perfect-scrollbar.min.js', true) }}"></script>
-    <script src="{{ asset('admin/js/plugins/smooth-scrollbar.min.js', true) }}"></script>
-    <script src="{{ asset('admin/js/plugins/chartjs.min.js', true) }}"></script>
+    <script src="{{ asset('admin/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('admin/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/js/plugins/chartjs.min.js') }}"></script>
 
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     {{-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> --}}
