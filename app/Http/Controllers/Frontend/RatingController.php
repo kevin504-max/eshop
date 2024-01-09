7848c4f82eccdbbece57667efd2df2308c95c6b4
Kevin Lucas <kevinbrito2012@gmail.com>
2024-01-09 12:16:54 -0300

    refact: turn rating to sql

--- app/Http/Controllers/Frontend/RatingController.php
+++ app/Http/Controllers/Frontend/RatingController.php
@@ -8,45 +8,43 @@ use App\Models\Product;
 use App\Models\Rating;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RatingController extends Controller
 {
     public function rate(Request $request)
     {
         try {
-            $product_check = Product::find($request->input('product_id'));
+            $productId = $request->input('product_id');
+            $product = DB::selectOne('SELECT * FROM products WHERE id = ?', [$productId]);
 
-            if (!$product_check) {
+            if (!$product) {
                 return redirect()->back()->with(['status' => 'error', 'message' => 'The link you followed was broken.']);
             }
 
-            $verified_purchase = Order::where('orders.user_id', Auth::id())
-                ->join('order_items', 'orders.id', 'order_items.order_id')
-                ->where('order_items.product_id', $product_check->id)->get();
+            $verifiedPurchase = DB::selectOne(
+                'SELECT * FROM orders
+                JOIN order_items ON orders.id = order_items.order_id
+                WHERE orders.user_id = ?
+                AND order_items.product_id = ?', [Auth::id(), $productId]
+            );
 
-            if (!$verified_purchase->count()) {
+            if (!$verifiedPurchase) {
                 return redirect()->back()->with(['status' => 'info', 'message' => 'You cannot rate a product without purchase.']);
             }
 
-            $existing_rating = Rating::where('user_id', Auth::id())->where('product_id', $request->input('product_id'))->first();
-
-            if (!$existing_rating) {
-                $data = [
-                    'user_id' => Auth::id(),
-                    'product_id' => $request->input('product_id'),
-                    'rating' => $request->input('product_rating')
-                ];
-
-                Rating::create($data);
-            } else {
-                $existing_rating->rating = $request->input('product_rating');
-                $existing_rating->update();
-            }
+            DB::statement("
+                    INSERT INTO ratings (user_id, product_id, rating, created_at, updated_at)
+                    VALUES (?, ?, ?, NOW(), NOW())
+                    ON DUPLICATE KEY UPDATE rating = VALUES(rating), updated_at = NOW()
+                ", [Auth::id(), $productId, $request->input('product_rating')]
+            );
 
             return redirect()->back()->with(['status' => 'success', 'message' => 'Thank you for your rating!']);
         } catch (\Throwable $th) {
-            report ($th);
+            report($th);
             return redirect()->back()->with(['status' => 'error', 'message' => 'Something went wrong! Try again.']);
         }
     }
+
 }
